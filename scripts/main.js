c3601845a843564647c7637eec580b048c5c2890
Jude Lawson <lawsonjdutton@gmail.com>
2018-09-25 20:33:04 -0600

    Updates currentTarget usage

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 const thumbnails = [...document.querySelectorAll('.thumbnail')]
 
 const routeTo = (event) => {
-  window.location.href = event.target.attributes['data-route'].value
+  window.location.href = event.currentTarget.attributes['data-route'].value
 }
 
 thumbnails.forEach(thumbnail => {
-  thumbnail.addEventListener('click', (e) => { routeTo(e) }, true)
+  thumbnail.addEventListener('click', (e) => { console.log(this); routeTo(e) }, true)
 })

04e7e3322bf97e56312d827251eaa326ddcd26af
Jude Lawson <lawsonjdutton@gmail.com>
2018-09-26 16:54:01 -0600

    Updates conteact page

--- scripts/main.js
+++ scripts/main.js
@@ -5,5 +5,13 @@ const routeTo = (event) => {
 }
 
 thumbnails.forEach(thumbnail => {
-  thumbnail.addEventListener('click', (e) => { console.log(this); routeTo(e) }, true)
+  if (thumbnail.id !== 'qs-api' || thumbnail.id !== 'rails-engine') {
+    thumbnail.addEventListener('click', (e) => { console.log(this); routeTo(e) }, true)
+  }
 })
+
+const qsApiThumbnail = document.querySelector('#qs-api')
+qsApiThumbnail.addEventListener('click', () => { window.location = 'https://github.com/jude-lawson/quantified-self-phoenix' })
+
+const railsEngineThumbnail = document.querySelector('#rails-engine')
+railsEngineThumbnail.addEventListener('click', () => { window.location = 'https://github.com/jude-lawson/rales_engine' })
